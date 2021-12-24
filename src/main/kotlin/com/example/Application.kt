499fcab0e88c42845c6fce84981a82808c015a7c
wakelift <xandr66@yandex.ru>
2021-12-24 05:24:22 +0300

    di

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,20 @@
 package com.example
 
-import io.ktor.application.*
+import com.example.di.mainModule
 import com.example.plugins.*
+import io.ktor.application.*
+import org.koin.ktor.ext.Koin
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+
+    install(Koin) {
+        modules(mainModule)
+    }
+
     configureRouting()
     configureSockets()
     configureSerialization()
